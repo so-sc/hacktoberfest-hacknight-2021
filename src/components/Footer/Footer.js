2757106efb42f78888cf589e05746d6b492da1b4
dhanushp <dhanushp.work@gmail.com>
2021-09-28 00:03:26 +0530

    Set up SCSS

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div>World</div>;
+};
+
+export default Footer;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hero = () => {
+  return <div>Hello world</div>;
+};
+
+export default Hero;
