09673e7b62b14cc46f5754911792606c9f923ebf
dhanushp <dhanushp.work@gmail.com>
2021-09-29 23:24:19 +0530

    Fixed hero svg

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,18 +6,24 @@ const Hero = () => {
   return (
     <div className={styles.heroSection}>
       <div className={styles.svg}>
-        <img src={require('../../assets/hero-left.svg').default} alt="left-svg" />
+        <img
+          src={require('../../assets/hero-left.svg').default}
+          alt='left-svg'
+        />
       </div>
       <Container>
         <div className={styles.heading}>
-          <img src={require('../../assets/logo.svg').default} alt="logo" />
+          <img src={require('../../assets/logo.svg').default} alt='logo' />
           <h1> 15 OCT - 17 OCT </h1>
-          <a href="/"> REGISTER </a>
+          <a href='/'> REGISTER </a>
           {/* <h1 className={styles.headingText}>Hacktoberfest Hacknight</h1> */}
         </div>
       </Container>
       <div className={styles.svg}>
-        <img src={require('../../assets/hero-right.svg').default} alt="right-svg" />
+        <img
+          src={require('../../assets/hero-right.svg').default}
+          alt='right-svg'
+        />
       </div>
     </div>
   );
