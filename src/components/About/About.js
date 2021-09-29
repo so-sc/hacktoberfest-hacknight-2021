d248f992d1b994c7ee87f8689e0042464bd4c38a
dhanushp <dhanushp.work@gmail.com>
2021-09-29 23:37:26 +0530

    Small fix in About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
 import Container from '../Container/Container';
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 import React from 'react';
 
 const About = () => {
@@ -9,10 +9,33 @@ const About = () => {
         <h1> About </h1>
         <div className={styles.content}>
           <p>
-            Open source is changing the world - <strong>one pull request</strong> at a time.<br/>Hack Night is a 36-hour long hackthon organized by <strong><a href="https://sosc.org.in/">Sahyadri Open Source Community (SOSC)</a></strong> in celebration of <strong><a href="https://hacktoberfest.digitalocean.com/">Hacktoberfest</a></strong> 2021. Hacktoberfest is open to everyone in our global community. Whether you’re new to development, a student or a long-time contributor, you can help drive growth of open source and make positive contributions to an ever-growing community. All backgrounds and skills levels are encouraged to complete the challenge.
+            Open source is changing the world -{' '}
+            <strong>one pull request</strong> at a time.
+            <br />
+            <br />
+            Hack Night is a 36-hour long hackthon organized by{' '}
+            <strong>
+              <a href='https://sosc.org.in/'>
+                Sahyadri Open Source Community (SOSC)
+              </a>
+            </strong>{' '}
+            in celebration of{' '}
+            <strong>
+              <a href='https://hacktoberfest.digitalocean.com/'>
+                Hacktoberfest
+              </a>
+            </strong>{' '}
+            2021. Hacktoberfest is open to everyone in our global community.
+            Whether you’re new to development, a student or a long-time
+            contributor, you can help drive growth of open source and make
+            positive contributions to an ever-growing community. All backgrounds
+            and skills levels are encouraged to complete the challenge.
           </p>
           <div>
-            <img src={require('../../assets/hackathon.png').default} alt="hackathon" />
+            <img
+              src={require('../../assets/hackathon.png').default}
+              alt='hackathon'
+            />
           </div>
         </div>
       </Container>
