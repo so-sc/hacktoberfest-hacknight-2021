62403a4bc59e45587862a362d9b94f3c42d545e8
fauwara <fawazabid96@gmail.com>
2021-10-06 01:31:45 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import Counter from './components/Counter/Counter';
-import Hero from './components/Hero/Hero';
-import About from './components/About/About';
-import Stats from './components/Stats/Stats';
-import Events from './components/Events/Events';
-import Sponsor from './components/Sponsor/Sponsor';
-import Layout from './components/Layout';
+import Home from './pages/home';
+import Register from './pages/register';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => (
   <div className='app'>
-    <Counter />
-    <Layout>
-      <Hero />
-      <About />
-      <Stats />
-      <Events />
-      <Sponsor />
-    </Layout>
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/register' component={Register} />          
+          <Route path='/' component={Home} />          
+        </Switch>
+      </div>
+    </Router>
   </div>
 );
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,38 +1,41 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import Container from '../Container/Container';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className={styles.heroSection}>
-      <div className={styles.svg}>
-        <img
-          className={styles.left}
-          src={require('../../assets/hero-side.svg').default}
-          alt='left-svg'
-        />
-      </div>
-      <Container>
-        <div className={styles.heading}>
-          <div className={styles.logo}><img src={require('../../assets/logo.svg').default} alt="logo" /></div>
-          <h1> HackNight </h1>
-          <h2> 22 OCT - 24 OCT </h2>
-          <div className={styles.register}>
-            <img className={styles.left} src={require('../../assets/small-flower.svg').default} alt="small-flower" />
-            <a href="/"> REGISTER </a>
-            <img className={styles.right} src={require('../../assets/small-flower.svg').default} alt="small-flower" />
-          </div>
-          {/* <h1 className={styles.headingText}>Hacktoberfest Hacknight</h1> */}
+    <Router>
+      <div className={styles.heroSection}>
+        <div className={styles.svg}>
+          <img
+            className={styles.left}
+            src={require('../../assets/hero-side.svg').default}
+            alt='left-svg'
+          />
         </div>
-      </Container>
-      <div className={styles.svg}>
-        <img
-          className={styles.right}
-          src={require('../../assets/hero-side.svg').default}
-          alt='right-svg'
-        />
+        <Container>
+          <div className={styles.heading}>
+            <div className={styles.logo}><img src={require('../../assets/logo.svg').default} alt="logo" /></div>
+            <h1> HackNight </h1>
+            <h2> 22 OCT - 24 OCT </h2>
+            <div className={styles.register}>
+              <img className={styles.left} src={require('../../assets/small-flower.svg').default} alt="small-flower" />
+                <Link to='/register'> REGISTER </Link>
+              <img className={styles.right} src={require('../../assets/small-flower.svg').default} alt="small-flower" />
+            </div>
+            {/* <h1 className={styles.headingText}>Hacktoberfest Hacknight</h1> */}
+          </div>
+        </Container>
+        <div className={styles.svg}>
+          <img
+            className={styles.right}
+            src={require('../../assets/hero-side.svg').default}
+            alt='right-svg'
+          />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
