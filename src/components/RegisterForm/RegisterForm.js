f4c396dbbcbb742894c8e72ac6642dc9e7e7f917
fauwara <fawazabid96@gmail.com>
2021-10-06 22:36:50 +0530

    added registration page

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,79 @@
+import { React, useState } from 'react';
+import Container from '../Container/Container';
+import styles from './styles.module.scss';
+
+const RegisterForm = () => {
+
+	const [inputs, setInputs] = useState("");
+
+	const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+	const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+
+	return (
+		<Container>
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<div className={styles.innerContainer}>
+					<h1>Register Now!</h1>
+					<label>
+						Name:
+						<input 
+        			type="text" 
+        			name="name" 
+        			value={inputs.name || ""}
+							onChange={handleChange}
+      			/>
+					</label>
+					<label>
+						Email:
+						<input 
+        			type="text" 
+        			name="email" 
+        			value={inputs.email || ""} 
+        			onChange={handleChange}
+      			/>
+					</label>
+					<label>
+						College:
+						<input 
+        			type="text" 
+        			name="college" 
+        			value={inputs.college || ""} 
+        			onChange={handleChange}
+      			/>
+					</label>
+					<label>
+						Phone Number:
+						<input 
+        			type="text" 
+        			name="phNumber" 
+        			value={inputs.phNumber || ""} 
+        			onChange={handleChange}
+      			/>
+					</label>
+					<label>
+						Discord Username:
+						<input 
+        			type="text" 
+        			name="discordId"
+        			value={inputs.discordId || ""} 
+        			onChange={handleChange}
+      			/>
+					</label>
+					<div className={styles.submit}>
+						<input type="submit" value="SUBMIT!" />
+					</div>
+				</div>
+			</form>
+		</Container>
+	);
+};
+
+export default RegisterForm;
