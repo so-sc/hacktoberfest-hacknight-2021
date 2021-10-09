974685a6168dee8f4497628ded91ad213314ed41
swaaz <swaasthik.shetty07@gmail.com>
2021-10-10 01:51:40 +0530

    completed registreation

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,126 +1,183 @@
-// import axios from 'axios';
-// import { React, useState } from 'react';
-import Container from '../Container/Container';
-import styles from './styles.module.scss';
-// const { REACT_APP_API } = process.env;
+import axios from "axios";
+import { React, useState } from "react";
+import Container from "../Container/Container";
+import styles from "./styles.module.scss";
+const { REACT_APP_API } = process.env;
 
 const RegisterForm = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    college: "",
+    phnNumber: "",
+    year: "",
+    discordId: "",
+    githubId: "",
+  });
 
-	// const [inputs, setInputs] = useState("");
-	
-	// const handleChange = (event) => {
-	// 	const name = event.target.name;
-	// 	const value = event.target.value;
-	// 	const e = value.length < 1 ? 'do not small ': '';
-	// 	setInputs(values => ({ ...values, [name]: value }));	
-	// }
-	
-	// const handleSubmit = (event) => {
-	// 	event.preventDefault();
-	// 	console.log(inputs);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
 
-	// 	axios.post(`${REACT_APP_API}/create/individual`, inputs)
-    //     .then( response => console.log(response))
-    //     .catch( error => console.log(error))
-		
-	// 	//popup
-	// 	let popup = document.getElementById('popup');
-	// 	// if the data is stored and everything is validated then show pop up
-	// 	popup.style.display = 'flex';
-		
-	// }
-	
-	// // const closePopup = () => {
-	// // 	let popup = document.getElementById('popup');
-	// // 	popup.style.display = 'none';
-	// // }
+    axios
+      .post(`${REACT_APP_API}/create/individual`, inputs)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
 
-	return (
-		<Container>
-			<form className={styles.form} 
-			// onSubmit={handleSubmit}
-			>
-				<div className={styles.innerContainer}>
-					<div className={styles.heading}>
-						<img className={styles.left} src={require('../../assets/small-flower.svg').default} alt="small-flower" />
-						<h1>Starting Tomorrow!</h1>
-						<img className={styles.right} src={require('../../assets/small-flower.svg').default} alt="small-flower" />
-					</div>
-					{/* <label>
-						Name:
-						<input
-							type="text"
-							name="name"
-							value={inputs.name || ""}
-							onChange={handleChange}
-						/>
-					</label>
-					<label>
-						Email:
-						<input
-							type="text"
-							name="email"
-							value={inputs.email || ""}
-							onChange={handleChange}
-						/>
-					</label>
-					<label>
-						College:
-						<input
-							type="text"
-							name="college"
-							value={inputs.college || ""}
-							onChange={handleChange}
-						/>
-					</label>
-					<label>
-						Phone Number:
-						<input
-							type="text"
-							name="phNumber"
-							value={inputs.phNumber || ""}
-							onChange={handleChange}
-						/>
-					</label>
-					<label>
-						Year:
-						<input
-							type="text"
-							name="year"
-							value={inputs.year || ""}
-							onChange={handleChange}
-						/>
-					</label>
-					<label>
-						Discord Username:
-						<input
-							type="text"
-							name="discordId"
-							value={inputs.discordId || ""}
-							onChange={handleChange}
-						/>
-					</label>
-					<label>
-						Github Username:
-						<input
-							type="text"
-							name="githubId"
-							value={inputs.githubId || ""}
-							onChange={handleChange}
-						/>
-					</label>
-					<div className={styles.submit}>
-						<input type="submit" value="SUBMIT!" />
-					</div> */}
+    //popup
+    let popup = document.getElementById("popup");
+    // if the data is stored and everything is validated then show pop up
+    popup.style.display = "flex";
+  };
 
-				</div>
+  const closePopup = () => {
+  	let popup = document.getElementById('popup');
+  	popup.style.display = 'none';
+  }
+  console.log(inputs);
 
-				{/* popup */}
-				
+  return (
+    <Container>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.innerContainer}>
+          <div className={styles.heading}>
+            <img
+              className={styles.left}
+              src={require("../../assets/small-flower.svg").default}
+              alt="small-flower"
+            />
+            <h1>Registration!</h1>
+            <img
+              className={styles.right}
+              src={require("../../assets/small-flower.svg").default}
+              alt="small-flower"
+            />
+          </div>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={inputs.name}
+              required={true} // add this to everything except for the Github
+              onChange={(v) => {
+                setInputs((values) => ({ ...values, name: v.target.value }));
+              }}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={inputs.email}
+              required={true} // add this to everything except for the Github
+              onChange={(v) => {
+                setInputs((values) => ({ ...values, email: v.target.value }));
+              }}
+            />
+          </label>
+          <label>
+            College:
+            <input
+              type="text"
+              name="college"
+              value={inputs.college}
+              required={true} // add this to everything except for the Github
+              onChange={(v) => {
+                setInputs((values) => ({ ...values, college: v.target.value }));
+              }}
+            />
+          </label>
+          <label>
+            Phone Number:
+            <input
+              type="tel"
+              name="phNumber"
+              value={inputs.phNumber}
+              required={true} // add this to everything except for the Github
+              onChange={(v) => {
+                setInputs((values) => ({
+                  ...values,
+                  phnNumber: v.target.value,
+                }));
+              }}
+            />
+          </label>
+          <label>
+            Year of Study:
+            <input
+              type="number"
+              name="year"
+              min="1"
+              max="4"
+              value={inputs.year}
+              required={true} // add this to everything except for the Github
+              onChange={(v) => {
+                setInputs((values) => ({ ...values, year: v.target.value }));
+              }}
+            />
+          </label>
+          <label>
+            Discord Username:
+            <input
+              type="text"
+              name="discordId"
+              value={inputs.discordId}
+              placeholder={"username#1234"}
+              pattern="[A-Za-z]*#[0-9]{4}"
+              required={true} // add this to everything except for the Github
+              onChange={(v) => {
+                setInputs((values) => ({
+                  ...values,
+                  discordId: v.target.value,
+                }));
+              }}
+            />
+          </label>
+          <label>
+            Github Username:
+            <input
+              type="text"
+              name="githubId"
+              value={inputs.githubId}
+              onChange={(v) => {
+                setInputs((values) => ({
+                  ...values,
+                  githubId: v.target.value,
+                }));
+              }}
+            />
+          </label>
+          <div className={styles.submit}>
+            <input type="submit" value="SUBMIT!" />
+          </div>
+        </div>
 
-			</form>
-		</Container>
-	);
+        {/* popup */}
+        <div id="popup" className={styles.popup}>
+          <div className={styles.content}>
+            <span className={styles.close} onClick={closePopup}>&times;</span>
+            <div className={styles.message}>
+              <img
+                className={styles.flower}
+                src={require("../../assets/small-flower.svg").default}
+                alt="flower"
+              />
+              <img
+              className={styles.checked}
+                src={require("../../assets/checked.png").default}
+                alt="checkmark"
+              />
+              <h1>Registered Successfully 🎉</h1>
+              <a href="https://discord.gg/2Dph95fvZW" target="_blank">Join our Discord</a>
+              <p>Since hack night this year is an online event join our Discord to take part in it.</p>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Container>
+  );
 };
 
 export default RegisterForm;
