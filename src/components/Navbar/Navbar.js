bef7eada13059c8d60d2b4eb2c11a30b41edad92
fauwara <fawazabid96@gmail.com>
2021-10-11 22:33:19 +0530

    fixed registration link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from '../Container/Container';
 import styles from './styles.module.scss';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -8,7 +9,7 @@ const Navbar = () => {
       <nav className={styles.navbar}>
         <ul>
           <li><a href="/">HOME</a></li>
-          <li><a href="/register">REGISTRATION</a></li>
+          <li><Link to="/register">REGISTRATION</Link></li>
           <li><a href="#contact">CONTACT</a></li>
         </ul>
       </nav>
