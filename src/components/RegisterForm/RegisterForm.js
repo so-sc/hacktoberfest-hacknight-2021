89b5236bc9f6978879c861c983ae69727fa39e44
fauwara <fawazabid96@gmail.com>
2021-10-18 21:29:58 +0530

    added tracks and rules

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { React, useState } from "react";
-import Container from "../Container/Container";
+import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
 import loaderImage from '../../assets/loading.gif';
 
@@ -30,23 +30,23 @@ const RegisterForm = () => {
   const handleSubmit = (event) => {
     setLoader(true);
     event.preventDefault();
-    
+
     axios
-    .post(`${REACT_APP_API}/create/individual`, inputs)
-    .then((response) => {
-      setSuccessMsg({
-        status: true,
-        id: response.data.id
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      setErrMsg({
-        status: true,
-        message: error.response.data.message
-      });
-    })
-    .finally(() =>setLoader(false))
+      .post(`${REACT_APP_API}/create/individual`, inputs)
+      .then((response) => {
+        setSuccessMsg({
+          status: true,
+          id: response.data.id
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrMsg({
+          status: true,
+          message: error.response.data.message
+        });
+      })
+      .finally(() => setLoader(false))
 
   };
 
@@ -59,184 +59,183 @@ const RegisterForm = () => {
   }
 
   return (
-    <Container>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.innerContainer}>
-          <div className={styles.heading}>
-            <img
-              className={styles.left}
-              src={require("../../assets/small-flower.svg").default}
-              alt="small-flower"
-            />
-            <h1>Registration!</h1>
-            <img
-              className={styles.right}
-              src={require("../../assets/small-flower.svg").default}
-              alt="small-flower"
-            />
-          </div>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={inputs.name}
-              required={true} // add this to everything except for the Github
-              onChange={(v) => {
-                setInputs((values) => ({ ...values, name: v.target.value }));
-              }}
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={inputs.email}
-              required={true} // add this to everything except for the Github
-              onChange={(v) => {
-                setInputs((values) => ({ ...values, email: v.target.value }));
-              }}
-            />
-          </label>
-          <label>
-            College:
-            <input
-              type="text"
-              name="college"
-              value={inputs.college}
-              required={true} // add this to everything except for the Github
-              onChange={(v) => {
-                setInputs((values) => ({ ...values, college: v.target.value }));
-              }}
-            />
-          </label>
-          <label>
-            Phone Number:
-            <input
-              type="tel"
-              name="phone"
-              value={inputs.phone}
-              required={true} // add this to everything except for the Github
-              onChange={(v) => {
-                setInputs((values) => ({
-                  ...values,
-                  phone: v.target.value,
-                }));
-              }}
-            />
-          </label>
-          <label>
-            Year of Study:
-            <input
-              type="number"
-              name="year"
-              min="1"
-              max="4"
-              value={inputs.year}
-              required={true} // add this to everything except for the Github
-              onChange={(v) => {
-                setInputs((values) => ({ ...values, year: v.target.value }));
-              }}
-            />
-          </label>
-          <label>
-            Discord Username:
-            <input
-              type="text"
-              name="discordUsername"
-              value={inputs.discordUsername}
-              placeholder={"username#1234"}
-              // pattern="[A-Za-z0-9]*#[0-9]{4}"
-              pattern="^((.+?)#\d{4})"
-              required={true} // add this to everything except for the Github
-              onChange={(v) => {
-                setInputs((values) => ({
-                  ...values,
-                  discordUsername: v.target.value,
-                }));
-              }}
-            />
-          </label>
-          <label>
-            Github Username:
-            <input
-              type="text"
-              name="githubUsername"
-              value={inputs.githubUsername}
-              onChange={(v) => {
-                setInputs((values) => ({
-                  ...values,
-                  githubUsername: v.target.value,
-                }));
-              }}
-            />
-          </label>
-          <div className={styles.submit}>
-            <input type="submit" value="SUBMIT!" />
-          </div>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.innerContainer}>
+        <div className={styles.heading}>
+          <img
+            className={styles.left}
+            src={require("../../assets/small-flower.svg").default}
+            alt="small-flower"
+          />
+          <h1>Registration!</h1>
+          <img
+            className={styles.right}
+            src={require("../../assets/small-flower.svg").default}
+            alt="small-flower"
+          />
         </div>
-          {
-            loader  && 
-            <div className={styles.loaderBG}>
-              <img className={styles.loader} src={loaderImage} alt="loader" />
-            </div>
-          }
-        {/* successfull registration */}
-        {
-          successMsg.status &&
-          <div id="success" className={styles.popup}>
-            <div className={styles.content}>
-              <span className={styles.close} onClick={closePopup}>&times;</span>
-              <div className={styles.message}>
-                <img
-                  className={styles.flower}
-                  src={require("../../assets/small-flower.svg").default}
-                  alt="flower"
-                  />
-                <img
+        <div className={styles.rules}>Make sure to read the rules <Link to='/rules'>here</Link> before registering.</div>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={inputs.name}
+            required={true} // add this to everything except for the Github
+            onChange={(v) => {
+              setInputs((values) => ({ ...values, name: v.target.value }));
+            }}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={inputs.email}
+            required={true} // add this to everything except for the Github
+            onChange={(v) => {
+              setInputs((values) => ({ ...values, email: v.target.value }));
+            }}
+          />
+        </label>
+        <label>
+          College:
+          <input
+            type="text"
+            name="college"
+            value={inputs.college}
+            required={true} // add this to everything except for the Github
+            onChange={(v) => {
+              setInputs((values) => ({ ...values, college: v.target.value }));
+            }}
+          />
+        </label>
+        <label>
+          Phone Number:
+          <input
+            type="tel"
+            name="phone"
+            value={inputs.phone}
+            required={true} // add this to everything except for the Github
+            onChange={(v) => {
+              setInputs((values) => ({
+                ...values,
+                phone: v.target.value,
+              }));
+            }}
+          />
+        </label>
+        <label>
+          Year of Study:
+          <input
+            type="number"
+            name="year"
+            min="1"
+            max="4"
+            value={inputs.year}
+            required={true} // add this to everything except for the Github
+            onChange={(v) => {
+              setInputs((values) => ({ ...values, year: v.target.value }));
+            }}
+          />
+        </label>
+        <label>
+          Discord Username:
+          <input
+            type="text"
+            name="discordUsername"
+            value={inputs.discordUsername}
+            placeholder={"username#1234"}
+            // pattern="[A-Za-z0-9]*#[0-9]{4}"
+            pattern="^((.+?)#\d{4})"
+            required={true} // add this to everything except for the Github
+            onChange={(v) => {
+              setInputs((values) => ({
+                ...values,
+                discordUsername: v.target.value,
+              }));
+            }}
+          />
+        </label>
+        <label>
+          Github Username:
+          <input
+            type="text"
+            name="githubUsername"
+            value={inputs.githubUsername}
+            onChange={(v) => {
+              setInputs((values) => ({
+                ...values,
+                githubUsername: v.target.value,
+              }));
+            }}
+          />
+        </label>
+        <div className={styles.submit}>
+          <input type="submit" value="SUBMIT!" />
+        </div>
+      </div>
+      {
+        loader &&
+        <div className={styles.loaderBG}>
+          <img className={styles.loader} src={loaderImage} alt="loader" />
+        </div>
+      }
+      {/* successfull registration */}
+      {
+        successMsg.status &&
+        <div id="success" className={styles.popup}>
+          <div className={styles.content}>
+            <span className={styles.close} onClick={closePopup}>&times;</span>
+            <div className={styles.message}>
+              <img
+                className={styles.flower}
+                src={require("../../assets/small-flower.svg").default}
+                alt="flower"
+              />
+              <img
                 className={styles.checked}
                 src={require("../../assets/checked.png").default}
-                  alt="checkmark"
-                />
-                <h1>Registered Successfully</h1>
-                <p
+                alt="checkmark"
+              />
+              <h1>Registered Successfully</h1>
+              <p
                 //  onClick={() => {console.log(successMsg.id)}}
 
-                onClick={() => {navigator.clipboard.writeText(`${successMsg.id}`)}}
-                className={styles.resgistrationID}>Registration ID: {successMsg.id}<span className={styles.resgistrationIDSpan}><img alt="copy" src="https://img.icons8.com/material-outlined/96/000000/copy.png"/></span></p>
-                <a href="https://discord.gg/2Dph95fvZW" rel="noreferrer" target="_blank">Join our Discord</a>
-                <p>Since HackNight this year is an online event join our Discord to take part in it. Save the Registration ID for future references*</p>
-              </div>
+                onClick={() => { navigator.clipboard.writeText(`${successMsg.id}`) }}
+                className={styles.resgistrationID}>Registration ID: {successMsg.id}<span className={styles.resgistrationIDSpan}><img alt="copy" src="https://img.icons8.com/material-outlined/96/000000/copy.png" /></span></p>
+              <a href="https://discord.gg/2Dph95fvZW" rel="noreferrer" target="_blank">Join our Discord</a>
+              <p>Since HackNight this year is an online event join our Discord to take part in it. Save the Registration ID for future references*</p>
             </div>
           </div>
-        }
-        
-        {/* failed registration */}
-        {
-          errMsg.status &&
-          <div id="fail" className={styles.popup}>
-            <div className={styles.content}>
-              <span className={styles.close} onClick={closePopup}>&times;</span>
-              <div className={styles.message}>
-                <img
-                  className={styles.flower}
-                  src={require("../../assets/small-flower.svg").default}
-                  alt="flower"
-                />
-                <img
+        </div>
+      }
+
+      {/* failed registration */}
+      {
+        errMsg.status &&
+        <div id="fail" className={styles.popup}>
+          <div className={styles.content}>
+            <span className={styles.close} onClick={closePopup}>&times;</span>
+            <div className={styles.message}>
+              <img
+                className={styles.flower}
+                src={require("../../assets/small-flower.svg").default}
+                alt="flower"
+              />
+              <img
                 className={styles.checked}
-                  src={require("../../assets/red-x.png").default}
-                  alt="checkmark"
-                />
-                <h1>Registration Failed</h1>
-                <p className={styles.errMsg}>{errMsg.message}</p>
-              </div>
+                src={require("../../assets/red-x.png").default}
+                alt="checkmark"
+              />
+              <h1>Registration Failed</h1>
+              <p className={styles.errMsg}>{errMsg.message}</p>
             </div>
           </div>
-        }
-      </form>
-    </Container>
+        </div>
+      }
+    </form>
   );
 };
 
---
+++ src/components/Tracks/TrackList.js
@@ -0,0 +1,24 @@
+const TrackList = [
+    {
+        title: 'EDUCATION',
+        text: "At SOSC, we're all about making learning and education better and fun! In this theme, think of how you can use the best of Technology to redefine conventional classroom setup and class lessons, for instance, to make concepts easier to understand and much more interesting. Anything under the umbrella of education is accepted. Your idea just needs to be innovative and fun!",
+        img: require('../../assets/tracks/education.svg').default,
+    },
+    {
+        title: 'HEALTH',
+        text: "Health is always the demand of the hour. While this demand is forever increasing, the progress and efficiency, not so much! With the ongoing pandemic, this relaization struck us like no other. In this theme, you're expected to come up with ideas to help better the medical infrastructure with a simple yet innovative project that could change the lives of hundreds or even millions!",
+        img: require('../../assets/tracks/health.svg').default,
+    },
+    {
+        title: 'OPEN THEME',
+        text: "We believe the best ideas are birthed when no limitations are placed whatsoever. And we don't want to limit our hackers in any way. In this theme, you're free to build anything that excites you with any tech stack of your choice! That's right! The only condition is....it has to be innovative! That's all.",
+        img: require('../../assets/tracks/open-theme.svg').default,
+    },
+    {
+        title: 'ENTERTAINMENT',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra feugiat nibh natoque feugiat facilisi nisi. Sit commodo etiam turpis tortor integer at non consectetur.',
+        img: require('../../assets/tracks/entertainment.svg').default,
+    },
+]
+
+export default TrackList;
