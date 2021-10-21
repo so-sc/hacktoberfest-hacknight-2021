24187fe49e06c4a4867a40b635056d47d27412e4
fauwara <fawazabid96@gmail.com>
2021-10-21 22:15:33 +0530

    added speakers

--- src/components/Events/EventList.js
+++ src/components/Events/EventList.js
@@ -8,12 +8,20 @@ export const eventList = [
         description:'Learning how to use GitHub is an essential skill that is used in a wide range of software development activities as well with a few management related activities. It can also be used a simple tool to host websites as well. Collaborative work is especially a lot more important in times like this, where we have to work from home and be in sync with our teams.',
     },
     {
+        title:"Cyber security and why it's important",
+        date: '22nd Oct, 10:00 AM - 11:30 AM',
+        speakerName: 'Viral Parmar',
+        speakerImg: require('../../assets/speakers/viral.jpg').default,
+        speakerLink: 'https://www.linkedin.com/in/viral-parmar-8402a04a/',
+        description:"Cyber security is the practice of defending computers, servers, mobile devices, electronic systems, networks, and data from malicious attacks. It's also known as information technology security or electronic information security. The term applies in a variety of contexts, from business to mobile computing.",
+    },
+    {
         title:'Getting the Hacker Spirit',
         date: '22nd Oct, 2:30 PM - 4:00 PM',
         speakerName: 'Harsh B.',
         speakerImg: require('../../assets/speakers/harsh.jpg').default,
         speakerLink: 'https://www.linkedin.com/in/bajpaiharsh/',
-        description:'how bunch of hackers change the world and what can we learn from them i.e what is this hacker spirit that people keep talking about and how it gave birth to what is open-source',
+        description:'The hacker culture is a subculture of individuals who enjoy the intellectual challenge of creatively overcoming the limitations of software systems to achieve novel and clever outcomes. The act of engaging in activities in a spirit of playfulness and exploration is termed "hacking."',
     },
     // {
     //     title:'Getting the Hacker Spirit',
